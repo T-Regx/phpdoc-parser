88fd155f47a03add4ac0b72457e25847601d53a8
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2022-11-15 22:54:16 +0100

    Add test for example presented in ReadMe.md, which by the way is an incorrect example

---
+++ test/ReadMe/Test.php
@@ -0,0 +1,72 @@
+<?php
+namespace Test\ReadMe;
+
+use Jasny\PHPDocParser\PHPDocParser;
+use Jasny\PhpdocParser\PhpDocumentor;
+use Jasny\PhpdocParser\Tag\FlagTag;
+use Jasny\PhpdocParser\Tag\Summary;
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    /**
+     * @test
+     */
+    public function test()
+    {
+        // given
+        $readMeExample = "/**
+ * The description of foo. This function does a lot of thing
+ *   which are described here.
+ *
+ * Some more text here.
+ *
+ * @important
+ * @uses FooReader
+ * @internal 
+ * @param string|callable \$first   This is the first param
+ * @param int             \$second  The second one
+ * @return void
+ * @throws InvalidArgumentException
+ * @throws DoaminException if first argument is not found
+ */";
+        $parser = new PHPDocParser(PhpDocumentor::tags()->with([
+            new Summary(),
+            new FlagTag('important')
+        ]));
+        // when
+        $meta = $parser->parse($readMeExample);
+        // then
+        $expected = [
+            'important' => true,
+            'uses' => [
+                'type' => 'FooReader'
+            ],
+            'internal' => true,
+            'params' => [
+                'first' => [
+                    'type' => "string|callable",
+                    'name' => "first",
+                    'description' => "This is the first param"
+                ],
+                'second' => [
+                    'type' => "int",
+                    'name' => "second",
+                    'description' => 'The second one',
+                ]
+            ],
+            'return' => [
+                'type' => 'void'
+            ],
+            'throws' => [
+                ['type' => 'InvalidArgumentException'],
+                ['type' => 'DoaminException', 'description' => 'if first argument is not found'],
+            ],
+            'summery' => "The description of foo. This function does a lot of thing",
+            'description' => "The description of foo. This function does a lot of thing
+which are described here.
+Some more text here.",
+        ];
+        $this->assertSame($expected, $meta);
+    }
+}
