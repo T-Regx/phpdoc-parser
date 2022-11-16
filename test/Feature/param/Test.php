87589fe39bd5b8d2e74cd4fb110a77167c5f4f67
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2022-11-16 20:43:40 +0100

    Add tests for @param

---
+++ test/Feature/param/Test.php
@@ -0,0 +1,49 @@
+<?php
+namespace Test\Feature\param;
+
+use PHPUnit\Framework\TestCase;
+use Test\Fixtures\ParseAssertion;
+use function Test\Fixture\resource;
+
+class Test extends TestCase
+{
+    use ParseAssertion;
+
+    /**
+     * @test
+     */
+    public function shouldParseParams()
+    {
+        // when, then
+        $this->assertParses(resource('param.many.txt'), [
+            'params' => [
+                'items'     => [
+                    'type'        => 'mixed[]',
+                    'name'        => 'items',
+                    'description' => "Array structure to count the elements of."
+                ],
+                'recursive' => [
+                    'type'        => 'bool',
+                    'name'        => 'recursive',
+                    'description' => "Optional."
+                ]
+            ]]);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldParseParamMultiline()
+    {
+        $this->markTestIncomplete();
+        // when, then
+        $this->assertParses(resource('param.multiline.txt'), [
+            'params' => [
+                'recursive' => [
+                    'type'        => 'bool',
+                    'name'        => 'recursive',
+                    'description' => "Optional. Whether or not to recursively\ncount elements in nested arrays.\nDefaults to `false`."
+                ]
+            ]]);
+    }
+}
